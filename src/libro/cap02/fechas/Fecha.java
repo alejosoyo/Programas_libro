b72a8f3291df3ada4228fb7ff45cad08c8fa3e5f
alejosoyo <jhon_perez23141@elpoli.edu.co>
2016-04-16 15:47:55 -0500

    Se agregaron los Set y Get

--- src/libro/cap02/fechas/Fecha.java
+++ src/libro/cap02/fechas/Fecha.java
@@ -6,4 +6,47 @@ public class Fecha
     private int mes;
     private int anio;
 
+    /**
+     * @return the dia
+     */
+    public int getDia() {
+        return dia;
+    }
+
+    /**
+     * @param dia the dia to set
+     */
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    /**
+     * @return the mes
+     */
+    public int getMes() {
+        return mes;
+    }
+
+    /**
+     * @param mes the mes to set
+     */
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    /**
+     * @return the anio
+     */
+    public int getAnio() {
+        return anio;
+    }
+
+    /**
+     * @param anio the anio to set
+     */
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+    
+    
 }
